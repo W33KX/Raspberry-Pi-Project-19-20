e06dd6a845178d5f6a118d8d5bd741c90f8c04c7
jb500 <s106480@ap.be>
2020-05-20 17:02:30 +0100

    gui push

--- GUI/test.py
+++ GUI/test.py
@@ -4,6 +4,7 @@ from tkinter import *
 from PIL import ImageTk,Image
 import time
 import random
+import paho.mqtt.client as paho
 
 hoogte_window=600
 breedte_window=1000
@@ -36,6 +37,40 @@ x_coordinaat = (breedte_scherm/2) - (breedte_window/2)
 y_coordinaat = (hoogte_scherm/2) - (hoogte_window/2)
 
 canvas = Canvas(window, width=breedte_scherm, height=hoogte_scherm)
+spelers=dict()
+
+def addSpeler(piname):
+ spelers[piname] = Wcrol(canvas, 10, 10)
+ print(spelers)
+ print("created: "+piname)
+
+def moveSpeler(piname, x, y):
+ print("move: "+piname)
+ spelers[piname].move(x, y)
+
+def on_message(client, userdata, msg):
+ message = msg.payload.decode("utf-8").split(";")
+ print(msg.topic + " " + str(msg.payload))
+ print("change" == message[0])
+ if True:
+   addSpeler(message[1])
+   print("created")
+ if message[0] == "position":
+   moveSpeler(message[3], message[1], message[2])
+
+def on_subsrcibe(client,userdat, mid, qos):
+ print("subscibed")
+
+
+client = paho.Client()
+client.on_message=on_message
+client.on_subscribe=on_subsrcibe
+client.username_pw_set("stef","stef")
+client.connect("rasberrypi.ddns.net", port=1883, keepalive=60)
+client.subscribe("project/#", qos=1)
+client.loop_start()
+
+
 canvas.pack()
 
 window.geometry("%dx%d+%d+%d" % (breedte_window, hoogte_window, x_coordinaat, y_coordinaat))
@@ -58,38 +93,37 @@ winkelkar = ImageTk.PhotoImage(winkelkar)
 winkelkar_foto = canvas.create_image(Xwaarde_spelerwinkelkar,Ywaarde_spelerwinkelkar, anchor=NE, image=winkelkar)
 
 class Wcrol:
- def __init__(self,canvas):
+ def __init__(self,canvas,x,y):
   self.canvas = canvas
-  self.id = canvas.create_image(Xwaarde_speler1,Ywaarde_speler1,anchor=NE, image=fotospeler)
-  self.canvas.move(self.id,200,100)
-  start = [-3,-2,-1,0,1,2,3]
-  #random richting -> momenteel nog
-  random.shuffle(start)
-  self.x = start[0]
-  self.y=-1
+  self.id = canvas.create_image(x,y,anchor=NE, image=fotospeler)
+  #self.canvas.move(self.id,x,y)
+  self.x = x
+  self.y = y
+  print(str(self.x) + "," + str(self.y))
 
 #coords geeft een array terug van [x1,y1,x2,y2]
  def draw(self):
-  self.canvas.move(self.id,self.x,self.y)
+  self.canvas.coords(self.id,self.x,self.y)
   pos = self.canvas.coords(self.id)
-  print(pos)
+  #print(pos)
+  print("draw: " + str(self.x) + "," +str(self.y) )
 
   #de wcrol moet begrensd worden ter hoogte van de X en Y assen
-  if pos[0]<=100:
-   self.x=1
-  if pos[1]<=0:
-   self.y = 1
-  if pos[0]>=1000:
-   self.x=-1
-  if pos[1]>=500:
-   self.y=-3
+ def move(self, x, y):
+  self.x=x
+  self.y=y
+  print("move")
 
-rol1 = Wcrol(canvas)
+rol1 = Wcrol(canvas, 10,10)
 
 while True:
  rol1.draw()
+ print(len(spelers))
+ for name in spelers:
+  spelers[name].draw()
  window.update_idletasks()
  time.sleep(0.01)
  window.update()
 
 window.mainloop()
+client.loop_stop()
